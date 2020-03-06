a9325aca7e4692fa334d0f3651dc88e6671e7dd5
NagashL0l <metridxblack@gmail.com>
2020-03-06 12:51:42 +0100

    AppBar pantallaCromo done

--- lib/cromo.dart
+++ lib/cromo.dart
@@ -1,8 +1,23 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class Cromo {
 
+  String nombre;
+  String descripcion;
+  double latitud;
+  double longitud;
+  String creador;
+  int numeroCromo;
 
-
+  Cromo({
+    @required this.nombre,
+    @required this.descripcion,
+    @required this.latitud,
+    @required this.longitud,
+    @required this.creador,
+    @required this.numeroCromo
+  });
 
 }
---
+++ lib/cromoWidget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:odissea/cromo.dart';
+
+class cromoWidget extends StatelessWidget{
+
+  final Cromo cromoPasado;
+
+  const cromoWidget({Key key, @required this.cromoPasado}) : super(key: key);
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(cromoPasado.nombre),
+        )
+      ),
+      
+
+    );
+  }
+
+
+}
