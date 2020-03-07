8c5246fbbbbc190fea8776347fea226c6c5a198d
NagashL0l <metridxblack@gmail.com>
2020-03-07 13:32:43 +0100

    Estadísticas done

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:odissea/tutorial.dart';
 
+import 'cromo.dart';
+import 'tutorial.dart';
+import 'vistaCromoWidget.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
